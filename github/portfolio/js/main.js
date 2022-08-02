e22e375e8b934b014f0e68d6fc5ccd9cf1e0338d
Diogo Guimarães <dj.ng@live.com.pt>
2022-08-02 23:05:15 +0100

    about me and responsive

---
+++ github/portfolio/js/main.js
@@ -0,0 +1,17 @@
+    /*const schools = ['./assets/flag.png','./assets/udemy.png','assets/computer.jpg']
+
+    let index = 0;
+
+    loop();
+
+    function loop(){
+        setTimeout(function() {
+            document.querySelector('.slider__image').src = schools[index];
+            if(index < 2){
+                index = index + 1;
+            }else{
+                index = 0;
+            }
+        loop();
+    }, 5000);
+    }*/
