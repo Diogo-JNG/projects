1d6620bcce8401e9b0bf11d10651e795833db23e
Diogo Guimarães <dj.ng@live.com.pt>
2022-08-27 17:21:22 +0100

    some visual improvements

--- github/streamingWebsite/js/main.js
+++ github/streamingWebsite/js/main.js
@@ -3,25 +3,22 @@ const aside = document.getElementById('aside');
 const main = document.getElementById('main');
 let videoThumb = document.querySelectorAll('.video__pre');
 
-// videoThumb.addEventListener('mouseover', () => {
-//    videoThumb.play();
-//});
 videoThumb.forEach((e) => {
-    e.addEventListener('mouseenter', () => {
-        e.play();
-    } );
+  e.addEventListener('mouseenter', () => {
+    e.play();
   });
+});
 
-  videoThumb.forEach((e) => {
-    e.addEventListener('mouseout', () => {
-        e.currentTime = 0;
-        e.pause();
-    } );
+videoThumb.forEach((e) => {
+  e.addEventListener('mouseout', () => {
+    e.currentTime = 0;
+    e.pause();
   });
+});
 
 burguerMenu.addEventListener('click', menuToggle);
 
-function menuToggle(){
-    main.classList.toggle('main__solo');
-    aside.classList.toggle('aside__hidden');
+function menuToggle() {
+  main.classList.toggle('main__solo');
+  aside.classList.toggle('aside__hidden');
 }
